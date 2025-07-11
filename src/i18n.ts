55dabb0d86e8c8678af6603625c675f54e768ae7
Thomaz R. Damasceno <thomazrdamasceno@gmail.com>
2025-07-11 12:12:42 +0000

    unhandledRejection Error [PageNotFoundError]: Cannot find module for pag

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,45 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+// Import translations directly
+import translationEN from '../public/locales/en/translation.json';
+import translationES from '../public/locales/es/translation.json';
+import translationJA from '../public/locales/ja/translation.json';
+import translationPT from '../public/locales/pt/translation.json';
+import translationZH from '../public/locales/zh/translation.json';
+
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  es: {
+    translation: translationES,
+  },
+  ja: {
+    translation: translationJA,
+  },
+  pt: {
+    translation: translationPT,
+  },
+  zh: {
+    translation: translationZH,
+  },
+};
+
+
 i18n
-  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     supportedLngs: ['pt', 'en', 'es', 'zh', 'ja'],
     fallbackLng: 'pt',
     detection: {
       order: ['localStorage', 'navigator', 'htmlTag'],
       caches: ['localStorage'],
       lookupLocalStorage: 'i18nextLng',
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
     },
     react: {
       useSuspense: false,

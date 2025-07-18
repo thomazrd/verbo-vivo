bf2b756feb1323d67164616527a8a52cb6282885
Thomaz R. Damasceno <thomazrdamasceno@gmail.com>
2025-07-18 21:27:09 +0000

    O botão proximo e anterior devem ficar mais evidentes

--- src/components/armor/BattleCarousel.tsx
+++ src/components/armor/BattleCarousel.tsx
@@ -73,8 +73,8 @@ export function BattleCarousel({ armor }: { armor: Armor }) {
                 </CarouselItem>
               ))}
             </CarouselContent>
-            <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 text-white bg-white/10 hover:bg-white/20 border-white/20" />
-            <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 text-white bg-white/10 hover:bg-white/20 border-white/20" />
+            <CarouselPrevious className="absolute left-2 sm:left-4 top-1/2 -translate-y-1/2 text-white bg-white/10 hover:bg-white/20 border-white/20 h-10 w-10 sm:h-12 sm:w-12" />
+            <CarouselNext className="absolute right-2 sm:right-4 top-1/2 -translate-y-1/2 text-white bg-white/10 hover:bg-white/20 border-white/20 h-10 w-10 sm:h-12 sm:w-12" />
           </Carousel>
       </div>
       

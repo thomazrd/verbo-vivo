3b07cd9e06d7bc5590bbd35476511d19b49994d5
Thomaz R. Damasceno <thomazrdamasceno@gmail.com>
2025-08-06 13:07:08 +0000

    Está faltando funções no menu... certifique de colocar todas...

--- src/components/layout/navigation-items.ts
+++ src/components/layout/navigation-items.ts
@@ -11,6 +11,11 @@ import {
   Users,
   BookUser,
   Share2,
+  Newspaper,
+  Smile,
+  BookOpenCheck,
+  LockKeyhole,
+  HandHeart,
 } from "lucide-react";
 import type { TFunction } from 'i18next';
 
@@ -18,15 +23,20 @@ export const mainNavItems = (t: TFunction) => [
   { href: "/home", label: t('nav_home'), icon: Home },
   { href: "/studies", label: t('nav_studies'), icon: GraduationCap },
   { href: "/community", label: t('nav_community'), icon: Users },
-  { href: "/journal", label: t('nav_journal'), icon: NotebookText },
   { href: "/armor", label: 'Minha Armadura', icon: Shield },
+  { href: "/bible", label: t('feature_bible_reader_title'), icon: BookMarked },
 ];
 
 export const secondaryNavItems = (t: TFunction) => [
     { href: "/chat", label: t('nav_chat'), icon: MessageSquare },
-    { href: "/bible", label: t('feature_bible_reader_title'), icon: BookMarked },
+    { href: "/journal", label: t('nav_journal'), icon: NotebookText },
     { href: "/prayer-circles", label: t('prayer_circles_title'), icon: HeartHandshake },
+    { href: "/plans", label: 'Meus Planos de Estudo', icon: BookOpenCheck },
+    { href: "/blog", label: "Artigos", icon: Newspaper },
     { href: "/characters", label: t('feature_characters_title'), icon: BookUser },
     { href: "/ponte-da-esperanca", label: t('feature_hope_bridge_title'), icon: Share2 },
+    { href: "/feeling-journey", label: t('feature_feeling_journey_title'), icon: Smile },
+    { href: "/confession", label: 'Confessionário', icon: LockKeyhole },
+    { href: "/faith-confession", label: 'Confissão de Fé', icon: HandHeart },
     { href: "/settings", label: t('nav_settings'), icon: Settings },
 ];

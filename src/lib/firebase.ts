3ac8b6ee7043429dd1b7fc694250f8ab910c42c9
Thomaz R. Damasceno <thomazrdamasceno@gmail.com>
2025-08-11 15:58:19 +0000

    E o cache do firestore... como fica isso...

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -19,5 +20,17 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const googleProvider = new GoogleAuthProvider();
+
+// Enable offline persistence
+enableIndexedDbPersistence(db).catch((err) => {
+    if (err.code == 'failed-precondition') {
+        // Multiple tabs open, persistence can only be enabled in one tab at a time.
+        console.warn("Firestore offline persistence failed: Multiple tabs open.");
+    } else if (err.code == 'unimplemented') {
+        // The current browser does not support all of the
+        // features required to enable persistence
+        console.warn("Firestore offline persistence failed: Browser not supported.");
+    }
+});
 
 export { app, auth, db, storage, googleProvider };
